591526c61ba23ae94ef7e0ab1c3004d9c590f1d6
Lu-Yi Shi <cloudx007@gmail.com>
2021-03-02 23:33:56 +0800

    Listing Brew Data

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_brew_crew_firebase/models/brew.dart';
+import 'package:flutter_brew_crew_firebase/screens/home/brew_tile.dart';
 import 'package:provider/provider.dart';
 
 class BrewList extends StatefulWidget {
@@ -12,12 +13,17 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context);
-    brews.forEach((brew) {
-      print(brew.name);
-      print(brew.sugars);
-      print(brew.strength);
-    });
+    // brews.forEach((brew) {
+    //   print(brew.name);
+    //   print(brew.sugars);
+    //   print(brew.strength);
+    // });
 
-    return Container();
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
