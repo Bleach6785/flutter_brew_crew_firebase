d4baf9249177775c5fb7c10a7882f4a43234648e
Lu-Yi Shi <cloudx007@gmail.com>
2021-03-02 11:22:29 +0800

    Register With Email & Password

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -11,10 +11,12 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -37,12 +39,14 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               SizedBox(
                 height: 20.0,
               ),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() {
                     email = val;
@@ -53,6 +57,8 @@ class _RegisterState extends State<Register> {
                 height: 20.0,
               ),
               TextFormField(
+                validator: (val) =>
+                    val.length < 6 ? 'Enter a password 6+ chars long' : null,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -70,9 +76,26 @@ class _RegisterState extends State<Register> {
                   ),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    // print(email);
+                    // print(password);
+                    dynamic result = await _auth.registerWithEnailAndPassword(
+                        email, password);
+                    if (result == null) {
+                      setState(() => error = 'please supply a valid email');
+                    } else {}
+                  }
                 },
+              ),
+              SizedBox(
+                height: 12.0,
+              ),
+              Text(
+                error,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14.0,
+                ),
               ),
             ],
           ),
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -31,6 +31,19 @@ class AuthService {
   // sign in with email & password
 
   // register with email & password
+  Future registerWithEnailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   // sign out
   Future signOut() async {
